a1f0fb62047f01c4c921ac2c37429069f8aee7ba
MarkSmith <marks@sencom.com.au>
2020-11-20 16:37:25 +0800

    Added menu system for selecting items

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Menu : MonoBehaviour
+{
+    public bool Active;
+    public Vector2 ActivePosition;
+    public Vector2 InactivePosition;
+    public float ScrollSpeed;
+    public RectTransform MenuBackground;
+    public EventSystem CanvasEvents;
+    private GameObject lastSelected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastSelected = CanvasEvents.firstSelectedGameObject;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleMenu();
+        }
+        if (Active)
+        {
+            MenuBackground.anchoredPosition = Vector2.Lerp(MenuBackground.anchoredPosition, ActivePosition, ScrollSpeed * Time.deltaTime);
+            CanvasEvents.sendNavigationEvents = true;
+            if(CanvasEvents.currentSelectedGameObject!= null)
+            {
+                lastSelected = CanvasEvents.currentSelectedGameObject;
+            }
+
+        }
+        else
+        {
+            MenuBackground.anchoredPosition = Vector2.Lerp(MenuBackground.anchoredPosition, InactivePosition, ScrollSpeed * Time.deltaTime);
+            CanvasEvents.sendNavigationEvents = false;
+
+        }
+    }
+
+    public void ToggleMenu()
+    {
+        if (Active)
+        {
+            Active = false;
+            GameManager.Paused = false;
+        }
+        else
+        {
+            Active = true;
+            GameManager.Paused = true;
+            CanvasEvents.SetSelectedGameObject(lastSelected);
+        }
+    }
+}
