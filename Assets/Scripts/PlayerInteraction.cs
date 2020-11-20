67c96def604cd3b0f186bb9c459b0ec232d3b55c
MarkSmith <marks@sencom.com.au>
2020-11-20 13:19:42 +0800

    Initial commit of all assets

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public bool Debug = true;
+    public GameObject DebugCube;
+    public MeshRenderer CubeMesh;
+    public Transform PlayerMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Paused)
+        {
+            return;
+        }
+
+        if (Debug)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                DebugCube.SetActive(true);
+                var playerPos = PlayerMesh.position + PlayerMesh.forward * 1;
+                var snapPos = new Vector3(Mathf.Round(playerPos.x), Mathf.Round(playerPos.y), Mathf.Round(playerPos.z));
+                DebugCube.transform.position = snapPos;
+                CubeMesh.enabled = true;
+            }
+            else
+            {
+                DebugCube.SetActive(false);
+            }
+        }
+        else
+        {
+            CubeMesh.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float MoveSpeed;
+    private CharacterController cc;
+
+    public Transform PlayerTransform;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Paused)
+        {
+            return;
+        }
+        
+
+        var xInput = Input.GetAxis("Horizontal");
+        var zInput = Input.GetAxis("Vertical");
+        var inputVector = new Vector3(xInput, 0, zInput).normalized;
+
+        cc.Move(transform.forward * inputVector.z * MoveSpeed * Time.deltaTime);
+        cc.Move(transform.right * inputVector.x * MoveSpeed * Time.deltaTime);
+        if(inputVector.magnitude > 0)
+        {
+            PlayerTransform.rotation = Quaternion.LookRotation(inputVector);
+        }
+
+
+
+
+
+    }
+}
