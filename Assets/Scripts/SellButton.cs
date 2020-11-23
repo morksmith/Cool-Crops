b64293f2299364738ef7a011cb1cbfa0d3c0ad0c
MarkSmith <marks@sencom.com.au>
2020-11-23 08:03:52 +0800

    Added "Sell All" functionality to sell screen

--- Assets/Scripts/SellButton.cs
+++ Assets/Scripts/SellButton.cs
@@ -7,19 +7,43 @@ public class SellButton : MonoBehaviour
 {
     public PlayerInventory Inventory;
     public GameObject FruitType;
+    public GameObject ApplePrefab;
+    public GameObject BananaPrefab;
+    public GameObject CarrotPrefab;
+    public bool SellAllButton = false;
     public Button MyButton;
 
     private Fruit fruitInfo;
+    private Fruit appleInfo;
+    private Fruit bananaInfo;
+    private Fruit carrotInfo;
     // Start is called before the first frame update
     void Start()
     {
-        fruitInfo = FruitType.GetComponent<Fruit>();
-
+        if (!SellAllButton)
+        {
+            fruitInfo = FruitType.GetComponent<Fruit>();
+        }
+        appleInfo = ApplePrefab.GetComponent<Fruit>();
+        bananaInfo = BananaPrefab.GetComponent<Fruit>();
+        carrotInfo = CarrotPrefab.GetComponent<Fruit>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (SellAllButton)
+        {
+            if (PlayerPrefs.GetInt("Apples") + PlayerPrefs.GetInt("Bananas") + PlayerPrefs.GetInt("Carrots") > 0)
+            {
+                MyButton.interactable = true;
+            }
+            else
+            {
+                MyButton.interactable = false;
+            }
+            return;
+        }
         if(PlayerPrefs.GetInt(fruitInfo.Type.ToString())> 0)
         {
             MyButton.interactable = true;
@@ -35,5 +59,14 @@ public class SellButton : MonoBehaviour
         Inventory.SetMoney(fruitInfo.Price);
         Debug.Log(PlayerPrefs.GetInt(fruitInfo.Type.ToString()));
 
+    }
+    public void SellAll()
+    {
+        var totalMoney = (PlayerPrefs.GetInt("Apples") * appleInfo.Price) + (PlayerPrefs.GetInt("Bananas") * bananaInfo.Price) + (PlayerPrefs.GetInt("Carrots") * carrotInfo.Price);
+        Inventory.SetMoney(totalMoney);
+        PlayerPrefs.SetInt("Apples", 0);
+        PlayerPrefs.SetInt("Bananas", 0);
+        PlayerPrefs.SetInt("Carrots", 0);
+        Debug.Log(PlayerPrefs.GetInt("Money"));
     }
 }
