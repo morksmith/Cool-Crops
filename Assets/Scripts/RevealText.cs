5adb472714744914fb489972a618049359e79788
MarkSmith <marks@sencom.com.au>
2020-11-30 09:47:41 +0800

    Added multi-sentence support to dialogue mechanic

--- Assets/Scripts/RevealText.cs
+++ Assets/Scripts/RevealText.cs
@@ -6,17 +6,19 @@ using TMPro;
 
 public class RevealText : MonoBehaviour
 {
-    private TextMeshProUGUI tmpro;
+    public TextMeshProUGUI DialogueText;
+    public List<string> Sentences;
     public bool Complete = false;
     public float RevealSpeed = 5;
     private float revealTimer;
     private int currentChar;
+    private int sentenceInt = 0;
     // Start is called before the first frame update
     void Start()
     {
-        tmpro = GetComponent<TextMeshProUGUI>();
-        tmpro.maxVisibleCharacters = 0;
+        DialogueText.maxVisibleCharacters = 0;
         currentChar = 0;
+        DialogueText.text = Sentences[0];
     }
 
     // Update is called once per frame
@@ -24,18 +26,45 @@ public class RevealText : MonoBehaviour
     {
         if (!Complete)
         {
-            revealTimer += Time.deltaTime * RevealSpeed;
-            if(revealTimer > 1)
+            if(DialogueText.maxVisibleCharacters < DialogueText.text.Length)
             {
-                tmpro.maxVisibleCharacters += 1;
-                revealTimer = 0;
-                Debug.Log(tmpro.text[currentChar]);
-                if (currentChar < tmpro.text.Length - 1)
+                revealTimer += Time.deltaTime * RevealSpeed;
+                if (revealTimer > 1)
                 {
-                    currentChar++;
+                    DialogueText.maxVisibleCharacters += 1;
+                    revealTimer = 0;
+                    Debug.Log(DialogueText.text[currentChar]);
+                    if (currentChar < DialogueText.text.Length - 1)
+                    {
+                        currentChar++;
+                    }
                 }
             }
+            else
+            {
+                Complete = true;
+            }
 
+        }
+        else if (Input.GetButtonDown("Fire1"))
+        {
+            NextSentence();
+        }
+    }
+
+    public void NextSentence()
+    {
+        if(sentenceInt < Sentences.Count - 1)
+        {
+            sentenceInt++;
+            DialogueText.text = Sentences[sentenceInt];
+            DialogueText.maxVisibleCharacters = 0;
+            currentChar = 0;
+            Complete = false;
+        }
+        else
+        {
+            Debug.Log("Close Dialogue");
         }
     }
 }
